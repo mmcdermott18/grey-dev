7a94c9b2b89db3cdbd2b822ad8b496cfaf40214c
mmcdermott18 <mmcdermott18@users.noreply.github.com>
2025-06-30 22:00:02 -0400

    updates!

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
     // run function on initial page load
-    // nav();
-    randomList();
     gallery();
     // run function on resize of the window
     $(window).resize(function() {
@@ -33,7 +31,6 @@ function gallery () {
   }
 
   var images = getImages("images.txt").split('\n');
-  console.log(images);
 
   function getCaptions(location)
   {
@@ -54,66 +51,33 @@ function gallery () {
   }
 
   var captions = getCaptions("captions.txt").split('\n');
-  console.log(captions);
-  var counter = 1;
+  var counter = 0;
   var end = images.length - 1;
   for (var i = 0; i < images.length; i++) {
+    var slideID = "#slide-" + i;
+    $('#home').prepend("<div class='gallery' id='slide-" + (i) + "'></div>" )
     $('#dots').append("<div class='dot'></div>");
+    $(slideID).css('background-image', 'url(assets/images/gallery/' + images[i]);
   }
-  $('#gallery').css('background-image', 'url(assets/images/gallery/' + images[0]);
   $('.caption').text(captions[0]);
-  $('.dot:nth-child(' + counter + ')').addClass('currentImage');
-  $('#gallery').click(function(){
-    $('.dot:nth-child(' + (end + 1) + ')').removeClass('currentImage');
-    $('#gallery').css('background-image', 'url(assets/images/gallery/' + images[counter]);
-    $('.caption').text(captions[counter]);
-    $('.dot:nth-child(' + (counter) + ')').removeClass('currentImage');
-    $('.dot:nth-child(' + (counter + 1) + ')').addClass('currentImage');
+  $('.dot:nth-child(' + (counter + 1) + ')').addClass('currentImage');
+  $('.gallery').click(function(){
+    var imgOld = "#slide-" + counter;
+    var imgNew = "#slide-" + (counter + 1);
+    $('.caption').text(captions[(counter + 1)]);
+    $('.dot:nth-child(' + (counter + 1) + ')').removeClass('currentImage');
+    $('.dot:nth-child(' + (counter + 2) + ')').addClass('currentImage');
+    $(imgOld).fadeOut(500);
+    $(imgNew).fadeIn(500);
     counter++;
     if (counter > end) {
       counter = 0;
+      var imgOld = "#slide-" + counter;
+      var imgNew = "#slide-" + (counter + 1);
+      $(imgOld).fadeIn(500);
+      $('.caption').text(captions[(counter)]);
+      $('.dot:nth-child(' + (end + 1) + ')').removeClass('currentImage');
+      $('.dot:nth-child(' + (counter + 1) + ')').addClass('currentImage');
     }
   })
-}
-
-// function nav() {
-//   $('.mobile-nav').click(function(){
-//     $('#menu').removeClass('mobile-hide');
-//     $('body').addClass('scroll-lock');
-//     $('#menu').click(function(){
-//       $('#menu').addClass('mobile-hide');
-//       $('body').removeClass('scroll-lock');
-//     });
-//   });
-// }
-
-function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
-
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-
-  return array;
-}
-
-function randomList(){
-  var students = ["Aileen", "Alex", "Bridget", "Dawson", "Giselle", "Izaac", "Jacob", "Jennifer", "Jessica", "Joy", "Meghan", "Nancy", "Raquel", "Reva", "Sal", "Sopheak", "Sung-Ho", "Thia", "Yaz"];
-  var y;
-  $('#generate').click( function(){
-    $('ol').empty()
-    shuffle(students);
-    for (y = 0; y < students.length; y++) {
-      var html = '<li>' + (y + 1) + '. ' + students[y] + '</li>';
-      $('#list').append(html);
-    };
-  });
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     // sliderWidths();
     // run function on resize of the window
     $(window).resize(function() {
-
+      sliderWidths();
     })
     // run function on scroll
     $(window).scroll(function() {
@@ -17,7 +17,6 @@ $(document).ready(function() {
 
 function sliderWidths () {
   var articles = $('article').length;
-  console.log(articles);
   for(i = 0; i < articles; i++) {
     var articleNum = i + 1;
     var $imageContainer = $('#project-' + articleNum); 
@@ -30,21 +29,17 @@ function sliderWidths () {
     // Loop through the first 5 images (or all if less than 5)
     $images.each(function(index) {
       var imageWidth = $(this).width() + 20;
-      console.log("Image " + (index + 1) + " width: " + imageWidth + "px");
       // You can also store the widths in an array if needed:
       imageWidths.push(imageWidth);
-      console.log(imageWidths);
     });
 
     let divWidth = 0;
     for (let i = 0; i < imageWidths.length; i++) {
       divWidth += imageWidths[i];
     }
-    console.log(divWidth);
     var mainF = $('main').outerWidth(true) - $('main').outerWidth();
     var mainH = mainF / 2;
-    console.log(mainH);
-    $imageContainer.css('width', divWidth + mainH + 'px');
+    $imageContainer.css('width', (divWidth + mainH - 6) + 'px');
   } 
 }
 
