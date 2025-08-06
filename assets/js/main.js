7f4452fa1565128b69ddb6f6808ceff33a46ee3a
mmcdermott18 <mmcdermott18@users.noreply.github.com>
2025-08-05 22:02:22 -0400

    grey fade in

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,24 @@
+// Helper function
+let domReady = (cb) => {
+  document.readyState === 'interactive' || document.readyState === 'complete'
+    ? cb()
+    : document.addEventListener('DOMContentLoaded', cb);
+};
+
+domReady(() => {
+  // Display body when DOM is loaded
+  setTimeout(function () {
+    document.body.style.visibility = 'visible';
+  }, 100);
+});
+
 $(document).ready(function() {
     // run function on initial page load
     // nav();
     setTimeout(() => {
       sliderWidths();
     }, "1000");
+    preload();
     // sliderWidths();
     // run function on resize of the window
     $(window).resize(function() {
@@ -14,7 +29,25 @@ $(document).ready(function() {
 
     })
 });
-
+function preload () {
+  const cat = sessionStorage.getItem("visited");
+  if (cat == "yes") {
+    preloader.style.display = 'none'; // Or use a fade-out effect
+  } else {
+      setTimeout(function() {
+        const preloader = document.getElementById('preloader');
+        if (preloader) {
+          preloader.style.opacity = '0'; // Or use a fade-out effect
+          preloader.style.transition = 'opacity 2s ease';
+          setTimeout(function() {
+            preloader.style.display = 'none'; // Or use a fade-out effect
+            sessionStorage.setItem("visited", "yes");
+            console.log(cat);
+          }, 2000);
+        }
+      }, 2000);
+    }
+  }
 function sliderWidths () {
   var articles = $('article').length;
   for(i = 0; i < articles; i++) {
